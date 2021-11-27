508bb84ccf58ee7e0587889d0aad0f8a3bea79d5
aliaguiar <aline.wirth@gmx.de>
2021-11-27 14:27:06 +0100

    HTML und CSS fertig, sounds hochgeladen

--- L07/task_material/main.ts
+++ L07/task_material/main.ts
@@ -1,16 +1,22 @@
 namespace L07{
 
     //Sounds
-    var sounds: string[] = [];
-
-    var sound:HTMLAudioElement = new Audio ();
+    var sound1:HTMLAudioElement = new Audio ("assets/A.mp3");
+    var sound2:HTMLAudioElement = new Audio ("assets/C.mp3");
+    var sound3:HTMLAudioElement = new Audio ("assets/F.mp3");
+    var sound4:HTMLAudioElement = new Audio ("assets/G.mp3");
+    var sound5:HTMLAudioElement = new Audio ("assets/hihat.mp3");
+    var sound6:HTMLAudioElement = new Audio ("assets/kick.mp3");
+    var sound7:HTMLAudioElement = new Audio ("assets/laugh-1.mp3");
+    var sound8:HTMLAudioElement = new Audio ("assets/laugh-2.mp3");
+    var sound9:HTMLAudioElement = new Audio ("assets/snare.mp3");
     sound.play();
 
     var boxes: string[] = [];
 
     //Funktion 7.1
     function playsounds(){
-    document.querySelector("body").style.background = sounds[]; void;
+    document.querySelector("body").style.background = ; void;
 
     }
     
