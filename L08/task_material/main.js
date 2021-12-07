12c7a46804603caec82207daa22fac7f2955a0e0
aliaguiar <aline.wirth@gmx.de>
2021-12-07 22:02:47 +0100

    updates, passt immer noch nicht

--- L08/task_material/main.js
+++ L08/task_material/main.js
@@ -1,75 +1,76 @@
-var L08;
-(function (L08) {
-    // tslint:disable: typedef
-    //Sounds
-    var sound1 = new Audio("assets/hihat.mp3");
-    var sound2 = new Audio("assets/kick.mp3");
-    var sound3 = new Audio("assets/snare.mp3");
-    var sound4 = new Audio("assets/A.mp3");
-    var sound5 = new Audio("assets/C.mp3");
-    var sound6 = new Audio("assets/F.mp3");
-    var sound7 = new Audio("assets/G.mp3");
-    var sound8 = new Audio("assets/laugh-1.mp3");
-    var sound9 = new Audio("assets/laugh-2.mp3");
-    var key = 0;
-    var sounds = ["assets/hihat.mp3", "assets/kick.mp3", "assets/snare.mp3", "assets/A.mp3", "assets/C.mp3", "assets/F.mp3", "assets/G.mp3", "assets/laugh-1.mp3", "assets/laugh-2.mp3"];
-    var beat = [sound2, sound2, sound3];
-    var play = document.querySelector("#play");
-    var song = new Audio(beat[key]);
-    var recorder = document.querySelector("#record");
-    var max = beat.length - 1;
-    var num;
-    //Funktion 7.1 Button
-    function playSample(sound) {
-        sound.play();
-        console.log("sound");
-    }
-    document.querySelector("#box1").addEventListener("mousedown", function () { playSample(sound1); });
-    document.querySelector("#box2").addEventListener("mousedown", function () { playSample(sound2); });
-    document.querySelector("#box3").addEventListener("mousedown", function () { playSample(sound3); });
-    document.querySelector("#box4").addEventListener("mousedown", function () { playSample(sound4); });
-    document.querySelector("#box5").addEventListener("mousedown", function () { playSample(sound5); });
-    document.querySelector("#box6").addEventListener("mousedown", function () { playSample(sound6); });
-    document.querySelector("#box7").addEventListener("mousedown", function () { playSample(sound7); });
-    document.querySelector("#box8").addEventListener("mousedown", function () { playSample(sound8); });
-    document.querySelector("#box9").addEventListener("mousedown", function () { playSample(sound9); });
-    //Funktion 7.2 Beat
-    var laufzahl = 0;
-    function playBeat() {
-        setInterval(function () {
-            beat[laufzahl++].play();
-            if (laufzahl == 3) {
-                laufzahl = 0;
-            }
-        }, 500);
-    }
-    document.querySelector("#play").addEventListener("click", function () { playBeat(); });
-    //Funktion 8.3 Play/Pause
-    function playwithpause() {
-        if (play.getAttribute("class") == "fas fa-pause") {
-            play.setAttribute("class", "fas fa-play");
-            playBeat();
+// tslint:disable: typedef
+//Sounds
+var sound1 = new Audio("assets/hihat.mp3");
+var sound2 = new Audio("assets/kick.mp3");
+var sound3 = new Audio("assets/snare.mp3");
+var sound4 = new Audio("assets/A.mp3");
+var sound5 = new Audio("assets/C.mp3");
+var sound6 = new Audio("assets/F.mp3");
+var sound7 = new Audio("assets/G.mp3");
+var sound8 = new Audio("assets/laugh-1.mp3");
+var sound9 = new Audio("assets/laugh-2.mp3");
+var key = 0;
+var sounds = ["assets/hihat.mp3", "assets/kick.mp3", "assets/snare.mp3", "assets/A.mp3", "assets/C.mp3", "assets/F.mp3", "assets/G.mp3", "assets/laugh-1.mp3", "assets/laugh-2.mp3"];
+var beat = [sound2, sound2, sound3];
+var play = document.querySelector("#play");
+var song = new Audio(beat[key]);
+var recorder = document.querySelector("#record");
+var max = beat.length - 1;
+var num;
+//Funktion 7.1 Button
+function playSample(sound) {
+    sound.play();
+    console.log("sound");
+}
+document.querySelector("#box1").addEventListener("mousedown", function () { playSample(sound1); });
+document.querySelector("#box2").addEventListener("mousedown", function () { playSample(sound2); });
+document.querySelector("#box3").addEventListener("mousedown", function () { playSample(sound3); });
+document.querySelector("#box4").addEventListener("mousedown", function () { playSample(sound4); });
+document.querySelector("#box5").addEventListener("mousedown", function () { playSample(sound5); });
+document.querySelector("#box6").addEventListener("mousedown", function () { playSample(sound6); });
+document.querySelector("#box7").addEventListener("mousedown", function () { playSample(sound7); });
+document.querySelector("#box8").addEventListener("mousedown", function () { playSample(sound8); });
+document.querySelector("#box9").addEventListener("mousedown", function () { playSample(sound9); });
+document.querySelector(".fas fa-random").addEventListener("click", function () { randomsounds(); });
+document.querySelector(".fas fa-play").addEventListener("click", function () { playwithpause(); });
+document.querySelector(".fas fa-trash-alt").addEventListener("click", function () { deleteBeat(); });
+//Funktion 7.2 Beat
+var laufzahl = 0;
+function playBeat() {
+    setInterval(function () {
+        beat[laufzahl++].play();
+        if (laufzahl == 3) {
+            laufzahl = 0;
         }
-        else {
-            play.setAttribute("class", "fas fa-pause");
-            if (play.getAttribute("class") == "fas fa-play") {
-                song.pause();
-            }
-        }
+    }, 500);
+}
+document.querySelector("#play").addEventListener("click", function () { playBeat(); });
+//Funktion 8.3 Play/Pause
+function playwithpause() {
+    if (play.getAttribute("class") == "fas fa-pause") {
+        play.setAttribute("class", "fas fa-play");
+        playBeat();
+        console.log("play");
     }
-    //Funktion 8.6 Remix
-    function randomnum(max) {
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max + 1));
-    }
-    function remix() {
-        for (var i = 0; i <= beat.length; i++) {
-            sounds[i] = beat[randomnum(max)];
+    else {
+        play.setAttribute("class", "fas fa-pause");
+        if (play.getAttribute("class") == "fas fa-play") {
+            clearInterval(sounds);
         }
     }
-    //Funktion 8.7 Löschen
-    function deleteBeat() {
-        beat.length = 0;
+}
+//Funktion 8.6 Remix
+function randomsounds() {
+    num = setInterval(remix, 500);
+}
+function remix() {
+    for (var i = 0; i <= beat.length; i++) {
+        var j = (Math.floor(Math.random) * 9);
+        playSample(sounds[j]);
     }
-})(L08 || (L08 = {}));
+}
+//Funktion 8.7 Löschen
+function deleteBeat() {
+    beat.length = 0;
+}
 //# sourceMappingURL=main.js.map
--- L08/task_material/main.ts
+++ L08/task_material/main.ts
@@ -1,4 +1,4 @@
-namespace L08 {
+
 // tslint:disable: typedef
 //Sounds
     var sound1: HTMLAudioElement = new Audio ("assets/hihat.mp3");
@@ -15,7 +15,7 @@ namespace L08 {
     var sounds: string [] = ["assets/hihat.mp3", "assets/kick.mp3", "assets/snare.mp3", "assets/A.mp3", "assets/C.mp3", "assets/F.mp3", "assets/G.mp3", "assets/laugh-1.mp3", "assets/laugh-2.mp3"];
     var beat: HTMLAudioElement[] = [sound2, sound2, sound3];
     var play: HTMLElement = document.querySelector("#play");
-    var song: HTMLAudioElement = new Audio(beat[key]);
+    var song: HTMLElement = new Audio(beat[key]);
     var recorder = document.querySelector("#record");
     var max = beat.length - 1;
     
@@ -37,6 +37,10 @@ namespace L08 {
     document.querySelector("#box7").addEventListener("mousedown", function() {playSample(sound7); });
     document.querySelector("#box8").addEventListener("mousedown", function() {playSample(sound8); });
     document.querySelector("#box9").addEventListener("mousedown", function() {playSample(sound9); });
+    document.querySelector(".fas fa-random").addEventListener("click", function() {randomsounds(); });
+    document.querySelector(".fas fa-play").addEventListener("click", function() {playwithpause(); });
+    document.querySelector(".fas fa-trash-alt").addEventListener("click", function() {deleteBeat(); });
+
 
 //Funktion 7.2 Beat
     var laufzahl: number = 0;
@@ -58,32 +62,29 @@ namespace L08 {
     if (play.getAttribute("class") == "fas fa-pause") {
         play.setAttribute("class", "fas fa-play");
         playBeat();
+        console.log("play");
         
-
     }
     else {
         play.setAttribute("class", "fas fa-pause");
     
-        if (play.getAttribute("class") == "fas fa-play") { song.pause(); }
+        if (play.getAttribute("class") == "fas fa-play") { clearInterval(sounds); }
     }
 }
 
 //Funktion 8.6 Remix
-    function randomnum(max) {
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max + 1));
-}
+    function randomsounds(): void {
+       num  = setInterval(remix, 500);
+    }
+
     function remix() {
     for (var i = 0; i <= beat.length; i++) {
-        sounds[i] = beat[randomnum(max)];
-    }
-}
+        var j: number = (Math.floor(Math.random) * 9);
+        playSample(sounds[j]);
+    }}
 
 
 //Funktion 8.7 Löschen
     function deleteBeat(): void {
     beat.length = 0;
   }
-
-
-}
