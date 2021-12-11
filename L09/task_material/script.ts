456f1b580e1bc043521d7829711315b73a98f316
aliaguiar <aline.wirth@gmx.de>
2021-12-12 00:34:22 +0100

    idk why it's not working

--- L09/task_material/script.ts
+++ L09/task_material/script.ts
@@ -3,8 +3,14 @@ namespace L09 {
 //Variablen
 const attachedtasks: HTMLElement = document.getElementById("attachedtasks");
 const p: HTMLParagraphElement = document.createElement("p");
+const check: HTMLElement = document.createElement("checkmark");
 let input: HTMLInputElement = document.querySelector("#input");
 let eingabefeldtext: string;
+const trash: HTMLButtonElement = document.createElement("button");
+const listdiv: HTMLDivElement = document.createElement("div");
+
+let counter: number = 0;
+
 
 //Buttondruck
 document.querySelector("#addtaskbutton").addEventListener("click", function(): void { newtask(); });
@@ -14,6 +20,7 @@ function newtask(): void {
     console.log("something");
     p.textContent = input.value;
     attachedtasks.appendChild(p);
+    counter ++;
 }
 //Eingabefeld-text für newtask
 function newtext(): void {
@@ -27,7 +34,20 @@ function complete(): void {
         console.log("anything");
     }
 
-//
+//löschbutton
+trash.innerHTML = "<i class= 'fas minus-square'> </i>";
+trash.classList.add("trash");
+listdiv.appendChild(trash);
 
+//Task löschen
+function deletetask(): void {
+    input.value = "";
+    counter --;
+}
+
+//todo counter
+function count(): void {
+    document.querySelector(".total").innerHTML = "Task(s) total: " + "counter";
+}
 
 }
