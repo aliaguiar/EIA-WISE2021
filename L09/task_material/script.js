708f6a5ee9c84a93f1280a0d5b8048e418f173ff
aliaguiar <aline.wirth@gmx.de>
2021-12-14 15:45:29 +0100

    upgrades, people, upgrades

--- L09/task_material/script.js
+++ L09/task_material/script.js
@@ -3,45 +3,29 @@ var L09;
     //Variablen
     const attachedtasks = document.getElementById("attachedtasks");
     const p = document.createElement("p");
-    const check = document.createElement("checkmark");
     let input = document.querySelector("#input");
-    let eingabefeldtext;
-    const trash = document.createElement("button");
+    let eingabefeldtext = document.createElement("p");
     const listdiv = document.createElement("div");
     let counter = 0;
+    //Kindelement appenden
+    attachedtasks.appendChild(listdiv);
     //Buttondruck
-    document.querySelector("#addtaskbutton").addEventListener("click", function () { newtask(); });
+    document.querySelector("#addtaskbutton").addEventListener("click", newtask);
     //neues <p> Element in HTML hinzufügen
     function newtask() {
+        //counter jeweils updaten
+        counter++;
+        document.querySelector(".total").innerHTML = "Task(s) total: " + counter;
+        //Text eingegeben
         console.log("something");
         p.textContent = input.value;
-        attachedtasks.appendChild(p);
-        counter++;
-    }
-    //Eingabefeld-text für newtask
-    function newtext() {
-        if (input.value != "")
-            eingabefeldtext = input.value;
-        input.value = "";
-    }
-    //newtask in div "attachedtasks" einfügen
-    p.addEventListener("click", complete);
-    attachedtasks?.appendChild(p);
-    function complete() {
-        console.log("anything");
-    }
-    //löschbutton
-    trash.innerHTML = "<i class= 'fas minus-square'> </i>";
-    trash.classList.add("trash");
-    listdiv.appendChild(trash);
-    //Task löschen
-    function deletetask() {
-        input.value = "";
-        counter--;
-    }
-    //todo counter
-    function count() {
-        document.querySelector(".total").innerHTML = "Task(s) total: " + "counter";
+        //div machen
+        attachedtasks?.appendChild(p);
+        //Text in div appenden
+        listdiv.appendChild(eingabefeldtext);
+        //haeckchen appenden
+        let check = document.createElement("div");
+        check.className = "far fa-check-square";
     }
 })(L09 || (L09 = {}));
 //# sourceMappingURL=script.js.map
--- L09/task_material/script.ts
+++ L09/task_material/script.ts
@@ -3,51 +3,35 @@ namespace L09 {
 //Variablen
 const attachedtasks: HTMLElement = document.getElementById("attachedtasks");
 const p: HTMLParagraphElement = document.createElement("p");
-const check: HTMLElement = document.createElement("checkmark");
 let input: HTMLInputElement = document.querySelector("#input");
-let eingabefeldtext: string;
-const trash: HTMLButtonElement = document.createElement("button");
+let eingabefeldtext: HTMLElement = document.createElement("p");
 const listdiv: HTMLDivElement = document.createElement("div");
-
 let counter: number = 0;
 
+//Kindelement appenden
+attachedtasks.appendChild(listdiv);
 
 //Buttondruck
-document.querySelector("#addtaskbutton").addEventListener("click", function(): void { newtask(); });
+document.querySelector("#addtaskbutton").addEventListener("click", newtask);
 
 //neues <p> Element in HTML hinzufügen
 function newtask(): void {
+    //counter jeweils updaten
+    counter ++;
+    document.querySelector(".total").innerHTML = "Task(s) total: " + counter;
+    //Text eingegeben
     console.log("something");
     p.textContent = input.value;
-    attachedtasks.appendChild(p);
-    counter ++;
+        //div machen
+    attachedtasks?.appendChild(p);
+        //Text in div appenden
+    listdiv.appendChild(eingabefeldtext);
+        //haeckchen appenden
+    let check: HTMLElement = document.createElement("div");
+    check.className = "far fa-check-square";
 }
-//Eingabefeld-text für newtask
-function newtext(): void {
-    if (input.value != "") eingabefeldtext = input.value;
-    input.value = "";
-}
-//newtask in div "attachedtasks" einfügen
-p.addEventListener("click", complete);
-attachedtasks?.appendChild(p);
-function complete(): void {
-        console.log("anything");
-    }
+  
+    
 
-//löschbutton
-trash.innerHTML = "<i class= 'fas minus-square'> </i>";
-trash.classList.add("trash");
-listdiv.appendChild(trash);
-
-//Task löschen
-function deletetask(): void {
-    input.value = "";
-    counter --;
-}
-
-//todo counter
-function count(): void {
-    document.querySelector(".total").innerHTML = "Task(s) total: " + "counter";
-}
 
 }
